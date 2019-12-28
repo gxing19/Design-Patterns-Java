7c590f598df3a5aa5e698b11292ab9c8206c25d7
gxing <gxing19@163.com>
2019-12-29 00:10:31 +0800

    Filter

---
+++ src/main/java/com/designpatterns/behavioral/chain_responsibility/logger/MainLogger.java
@@ -0,0 +1,17 @@
+package com.designpatterns.behavioral.chain_responsibility.logger;
+
+public class MainLogger {
+
+    public static void main(String[] args) {
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        errorLogger.logMessage(AbstractLogger.INFO, "this is a info.");
+        errorLogger.logMessage(AbstractLogger.DEBUG, "this is a debug.");
+        errorLogger.logMessage(AbstractLogger.ERROR, "this is a error.");
+    }
+}
