aa2161c3a39aa559070524d16671d5c2ec701453
gxing <xing123456>
2021-06-18 18:12:11 +0800

    add bridge model

---
+++ src/main/java/com/designpatterns/structural/bridge/Client.java
@@ -0,0 +1,15 @@
+package com.designpatterns.structural.bridge;
+
+/**
+ * @author gxing
+ * @desc 客户端
+ * @date 2021/6/18
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        Implementor implementor = new ConcreteImplementorA();
+        Abstraction abstraction = new RefinedAbstraction(implementor);
+        abstraction.operation();
+    }
+}
